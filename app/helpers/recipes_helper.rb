a64cd6ce1f593e38216ebc9a2f75044e6234f534
Joel <joel.b.williams.is@gmail.com>
2017-12-23 20:32:58 -0600

    admin auth/auth && access

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,14 @@
 module RecipesHelper
+
+	def logged_in?
+		session[:user_id]
+	end
+
+	def current_user
+		User.find_by_id(session[:user_id])
+	end
+
+	def is_admin?(user)
+		User.find_by_id(user.id)
+	end
 end
