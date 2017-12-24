87cd0d3f79c2dbd7d36bb035c570901b6cd27512
Joel <joel.b.williams.is@gmail.com>
2017-12-23 19:45:13 -0600

    user model w/bcrypt & login page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   	resources :ingredients
   end
   
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   root 'recipes#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171125141739) do
+ActiveRecord::Schema.define(version: 20171224012119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20171125141739) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "yield"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password"
+    t.boolean  "admin",      default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   add_foreign_key "ingredients", "recipes"
